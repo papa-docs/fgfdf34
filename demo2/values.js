3de5542171aa21f9ffb9835c8f20bf283dbd603d
papa-docs <papa-docs1@proton.me>
2025-06-30 14:14:01 +0300

    Add files via upload

---
+++ demo2/values.js
@@ -0,0 +1,56 @@
+// values.js
+
+// данные 
+var fio               = "Игнатенко Назар Петрович";
+var fio_en            = "Ignatenko Nazar Petrovich";
+var birth             = "17.04.1984";
+var rnokpp            = "3078825956";
+
+var prava_number      = "GE653214"; // номер прав
+var university        = "ХНУ имени Каразина"; // Університет
+var fakultet          = "Физико-технический"; // Факультет
+var stepen_dip        = "Магістра";
+var univer_dip        = "ХНУ имени Каразина";
+var dayout_dip        = "01.07.2023";
+var special_dip       = "Прикладная математика";
+var number_dip        = "MT-545678";
+var live              = "м. Кривий Pir";
+var prava_date_out    = "01.01.2030";
+var bank_adress       = "Обл. Днiпропетровська, м. Кривий Pir, ул. Лiтке, буд. 85";
+var sex               = "М";
+var sex_en            = "M";
+
+var zagran_number     = "GE653214";
+var student_number    = "2022154258";
+var student_date_give = "01.09.2021";
+var student_date_out  = "30.06.2025";
+var form              = "Очная";
+var rights_categories = "A, B";
+var date_give         = "14.12.2022";
+var prava_date_give   = "14.11.2007";
+var date_out          = "14.11.2032";
+var pass_number       = "GE653214";
+var organ             = "1223";
+var uznr              = "19840417-12236";
+var legalAdress       = "Обл. Днiпропетровська, м. Кривий Pir, ул. Лiтке, буд. 85";
+var registeredOn      = "15.03.2007";
+var pravaOrgan        = "ТСЦ 6345";
+
+
+// Включение/выключение документов
+var isStudyeDoc       = false;
+var isStudypasport    = false;
+var isRightsEnabled   = true;
+var isStudyEnabled    = false;
+var isZagranEnabled   = true;
+var isDiplomaEnabled  = true;
+// Добавь остальные переменные по аналогии
+
+// Пути к нужным фото    ?timestamp=N   "photo.jpg?timestamp=N" динамически: var url = "photo.jpg?timestamp=" + N;
+
+var photo_passport = "photo.jpg"; //эДок, паспорт,
+var photo_rights   = "photoR.png?timestamp=99"; //права
+var photo_students = "photo.jpg"; //диплом, студ
+var photo_zagran   = "photo.jpg"; //загран
+// Можно добавить еще, если надо
+
